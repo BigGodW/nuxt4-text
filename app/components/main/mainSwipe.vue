b2770e22c7c16f695f1c9f073b7eeb03dbbbe360
biggodw <biggodw@gmail.com>
2025-11-14 23:51:40 +0800

    修复001

--- app/components/main/mainSwipe.vue
+++ app/components/main/mainSwipe.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white" v-if="carousel">
+        <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white" >
             <van-swipe-item class=" relative" v-for="item in carousel">
                 <img class=" w-full aspect-16/6 object-cover" :src="useSupabaseImgUrl(item.image_url)" :alt="item.title">
                 <h1 class="text-center absolute bottom-0 left-0 right-0 font-semibold leading-5 bg-white/30 p-1">{{ item.title }}</h1>
@@ -9,8 +9,9 @@
     </div>
 </template>
 <script setup>
+const myCarousel = ref([])
 const {data:carousel}= await useAsyncData('getCarousel',async()=>{
     const res = await useSupabase().from('carousel').select().eq('is_active',true)
-    return res.data
+    myCarousel.value = res.data
 })
 </script>
